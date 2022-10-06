60a3a450f2b89d62770b34dba43154d6b5655ae2
Alass-sr <sarralass88@gmail.com>
2022-10-06 09:43:41 +0200

    ajout du crud post

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.vpghnia.mongodb.net/projet7back',
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
-}
-)
-.then(() => console.log('Connected MongoDB'))
-.catch((err) => console.log('Failed to connect MongoDB', err));
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.DB_USER_PASS +
+      "@cluster0.vpghnia.mongodb.net/projet7back",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("Connected MongoDB"))
+  .catch((err) => console.log("Failed to connect MongoDB", err));
